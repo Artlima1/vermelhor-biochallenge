944e099a18a7df194112f601468ca6068748a8bf
ArthurLima <arthurdeolima@gmail.com>
2024-06-18 19:37:51 -0300

    Intensity mode Log10

--- lightsaber/lightsaber.ino
+++ lightsaber/lightsaber.ino
@@ -1,18 +1,31 @@
-#define MAXDIST 300
-#define MINDIST 20
+#include <math.h>
+
+enum {
+  INTENSITY_MODE_LINEAR,
+  INTENSITY_MODE_LOG,
+};
+
+#define MAX_DIST 300
+#define MIN_DIST 20
+#define MIN_VIB 20
+#define MAX_VIB 255
 
 const int triggerPin = 9;
 const int ecoPin = 8;
 const int motorPin = 10;
 
-long duracao;
+long duration;
 float dist;
+int motor_intensity;
+int intensity_mode;
 
 void setup() {
   pinMode(triggerPin, OUTPUT); // Configura o pino trigger como saída
   pinMode(ecoPin, INPUT); // Configura o pino eco como entrada.
   pinMode(motorPin, OUTPUT);
   Serial.begin(9600); // Inicia a comunicação serial
+
+  intensity_mode = INTENSITY_MODE_LOG;
 }
 
 void loop() {
@@ -26,20 +39,25 @@ void loop() {
   digitalWrite(triggerPin, LOW);
 
   // Inicia contagem de tempo e lê o pino de eco
-  duracao = pulseIn(ecoPin, HIGH);
+  duration = pulseIn(ecoPin, HIGH);
 
   // Calcular a distância
-  dist = duracao * 0.034 / 2;
+  dist = duration * 0.034 / 2;
 
   // Mostrar a distância no monitor serial
-  if (dist >= MAXDIST) dist = MAXDIST;
-  if (dist <= MINDIST) dist = 0;
-  
+  if (dist > MAX_DIST) dist = MAX_DIST;
+  if (dist <= MIN_DIST) dist = 0;
 
-  Serial.print("Distancia em cm: ");
-  Serial.println(dist);
+  if(intensity_mode == INTENSITY_MODE_LINEAR) motor_intensity = MAX_VIB - ((dist*(MAX_VIB-MIN_VIB))/MAX_DIST);
+  else if(intensity_mode == INTENSITY_MODE_LINEAR) motor_intensity = MAX_VIB - (np.log10(dist*10.0/MAX_DIST))*(MAX_VIB-MIN_VIB);
 
-  analogWrite(motorPin, 255 - ((dist*240)/MAXDIST));
+  analogWrite(motorPin, motor_intensity);
+
+  Serial.print("Distancia: ");
+  Serial.print(dist);
+  Serial.print(" Intensity: ");
+  Serial.print(motor_intensity);
+  Serial.print("\n");
 
   // Aguardar 100ms antes da próxima leitura para evitar interferência
   delay(100);
