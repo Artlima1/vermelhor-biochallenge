dc8aec68b1633b99736baeae3d759cbf483134c5
ArthurLima <arthurdeolima@gmail.com>
2024-06-18 20:00:54 -0300

    Adding moving avg (LP Filter)

--- lightsaber/lightsaber.ino
+++ lightsaber/lightsaber.ino
@@ -10,6 +10,8 @@ enum {
 #define MIN_VIB 20
 #define MAX_VIB 255
 
+#define MOVING_AVG_LEN 20
+
 const int triggerPin = 9;
 const int ecoPin = 8;
 const int motorPin = 10;
@@ -18,6 +20,8 @@ long duration;
 float dist;
 int motor_intensity;
 int intensity_mode;
+float measures[MOVING_AVG_LEN];
+int curr_pos;
 
 void setup() {
   pinMode(triggerPin, OUTPUT); // Configura o pino trigger como saída
@@ -25,8 +29,11 @@ void setup() {
   pinMode(motorPin, OUTPUT);
   Serial.begin(9600); // Inicia a comunicação serial
 
-  intensity_mode = INTENSITY_MODE_LOG;
+  intensity_mode = INTENSITY_MODE_LINEAR;
+  curr_pos = 0;
 }
+
+float moving_avg();
 
 void loop() {
   // Limpa o trigger
@@ -42,14 +49,17 @@ void loop() {
   duration = pulseIn(ecoPin, HIGH);
 
   // Calcular a distância
-  dist = duration * 0.034 / 2;
+  measures[curr_pos] = duration * 0.034 / 2;
+  curr_pos = (curr_pos + 1) % MOVING_AVG_LEN;
+
+  dist = moving_avg();
 
   // Mostrar a distância no monitor serial
   if (dist > MAX_DIST) dist = MAX_DIST;
   if (dist <= MIN_DIST) dist = 0;
 
   if(intensity_mode == INTENSITY_MODE_LINEAR) motor_intensity = MAX_VIB - ((dist*(MAX_VIB-MIN_VIB))/MAX_DIST);
-  else if(intensity_mode == INTENSITY_MODE_LINEAR) motor_intensity = MAX_VIB - (np.log10(dist*10.0/MAX_DIST))*(MAX_VIB-MIN_VIB);
+  else if(intensity_mode == INTENSITY_MODE_LINEAR) motor_intensity = MAX_VIB - (log10(dist*10.0/MAX_DIST))*(MAX_VIB-MIN_VIB);
 
   analogWrite(motorPin, motor_intensity);
 
@@ -61,4 +71,15 @@ void loop() {
 
   // Aguardar 100ms antes da próxima leitura para evitar interferência
   delay(100);
+}
+
+float moving_avg(){
+  int i;
+  float avg;
+  for(i=0, avg=0; i<MOVING_AVG_LEN; i++){
+    avg += measures[i];
+  }
+  avg /= MOVING_AVG_LEN;
+
+  return avg;
 }
